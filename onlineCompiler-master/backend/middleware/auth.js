bf2de4dcb4fef9387d48602234df83962eb40ad9
Vijay kumar <vijayrajputjay14@gmail.com>
2025-09-16 16:23:48 +0530

    Update auth.js

--- onlineCompiler-master/backend/middleware/auth.js
+++ onlineCompiler-master/backend/middleware/auth.js
@@ -20,28 +20,3 @@ function auth(req, res, next) {
 
 module.exports = auth;
 
-
-
-
-// const config = require('config');
-// const jwt = require('jsonwebtoken');
-
-// function auth(req, res, next) {
-//     const authHeader = req.header('Authorization');
-
-//     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-//         return res.status(401).json({ msg: 'No token, authorization denied' });
-//     }
-
-//     const token = authHeader.split(' ')[1]; // Extract token
-
-//     try {
-//         const decoded = jwt.verify(token, config.get('jwtSecret'));
-//         req.user = decoded;
-//         next();
-//     } catch (e) {
-//         res.status(400).json({ msg: 'Token is not valid' });
-//     }
-// }
-
-// module.exports = auth;
